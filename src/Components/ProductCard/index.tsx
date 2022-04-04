df7a406cf3668102df28c5a63abb5e2890de99d8
Moloko20 <alexvfrontend@gmail.com>
2022-04-05 01:32:26 +0300

    refactor(product-card): add wrapper

--- src/Components/ProductCard/index.tsx
+++ src/Components/ProductCard/index.tsx
@@ -23,27 +23,37 @@ function ProductCardComponent({ product }: ProductCardProps) {
     const price = product.latest_price ? '0,0' + product.latest_price[0] : product.initial_price
 
     return (
-        <div className={styles.card}>
-            <div className={imageClassName}>
-                <div className={styles['card-nft__creator']}>
-                    <span>created by</span>
-                    <br />
-                    {product.created_by.display_name}
+        <div className={styles['card-wrapper']}>
+            <div className={styles.card}>
+                <div className={imageClassName}>
+                    <div className={styles['card-nft__creator']}>
+                        <span>created by</span>
+                        <br />
+                        {product.created_by.display_name}
+                    </div>
+                    <div className={styles['card-nft__name']}>{product.name}</div>
                 </div>
-                <div className={styles['card-nft__name']}>{product.name}</div>
-            </div>
-            <div className={styles['card-selling']}>
-                <div>
-                    available
-                    <br />
-                    <span>
-                        {product.quantity_available} of {product.quantity}
-                    </span>
-                </div>
-                <div>
-                    price
-                    <br />
-                    <span>{price} ETH</span>
+                <div className={styles['card-selling']}>
+                    <div>
+                        available
+                        <br />
+                        <span className={styles['card-selling_span']}>
+                            {product.quantity_available} of {product.quantity}
+                        </span>
+                    </div>
+                    <div>
+                        price
+                        <br />
+                        <span
+                            className={
+                                styles['card-selling_span'] +
+                                ' ' +
+                                styles['card-selling_span--price']
+                            }
+                        >
+                            {price} ETH
+                        </span>
+                    </div>
                 </div>
             </div>
         </div>
