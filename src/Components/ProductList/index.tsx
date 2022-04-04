411234966eaf47f59f05582eca81adfd9e072120
Moloko20 <alexvfrontend@gmail.com>
2022-04-05 00:37:59 +0300

    feat(product-list): add loader and error processing

--- src/Components/ProductList/index.tsx
+++ src/Components/ProductList/index.tsx
@@ -1,28 +1,42 @@
 import React from 'react'
 
 import { ProductCard } from 'Components/ProductCard'
+import { Loader } from 'Components/UI/Loader'
 
 import { getProducts, ProductItem } from 'Services/getProducts'
 
 import styles from './index.module.scss'
 
 function ProductListComponent() {
-    const [poductInfo, productInfoSet] = React.useState<Array<ProductItem>>([])
+    const [poductData, productDataSet] = React.useState<Array<ProductItem>>([])
+    const [poductErr, productErrSet] = React.useState<boolean>(false)
+
+    const [isLoaded, isLoadedSet] = React.useState<boolean>(false)
 
     React.useEffect(() => {
-        getProducts().then(data => {
-            console.dir(data)
-            productInfoSet(data.data.products)
-        })
+        getProducts()
+            .then(data => {
+                productDataSet(data.data.products)
+
+                isLoadedSet(true)
+            })
+            .catch(err => {
+                productErrSet(true)
+                console.error(err)
+            })
     }, [])
 
-    return (
-        <div className={styles.productlist}>
-            {poductInfo.map(product => (
-                <ProductCard key={product.product_id} product={product} />
-            ))}
-        </div>
-    )
+    const setTemplate = () => {
+        return poductErr ? (
+            <div className={styles['productlist-error']}>Something's gone wrong :(</div>
+        ) : isLoaded ? (
+            poductData.map(product => <ProductCard key={product.product_id} product={product} />)
+        ) : (
+            <Loader />
+        )
+    }
+
+    return <div className={styles['productlist']}>{setTemplate()}</div>
 }
 
 export const ProductList = React.memo(ProductListComponent)
