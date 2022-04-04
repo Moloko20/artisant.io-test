3d9740cb6be2bc7d0a4f0d61729e147817136301
Moloko20 <alexvfrontend@gmail.com>
2022-04-05 00:18:20 +0300

    feat(product-card): add new component

---
+++ src/Components/ProductCard/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+import { ProductItem } from 'Services/getProducts'
+
+import styles from './index.module.scss'
+
+type ProductCardProps = {
+    product: ProductItem
+}
+
+function ProductCardComponent({ product }: ProductCardProps) {
+    const setImageClass = (nftID: number) => {
+        if (nftID % 5 === 0) {
+            return styles['card-nft--eachfive']
+        } else if (nftID % 2 === 0) {
+            return styles['card-nft--even']
+        }
+        return styles['card-nft--odd']
+    }
+
+    const imageClassName: string = styles['card-nft'] + ' ' + setImageClass(product.product_id)
+
+    const price = product.latest_price ? '0,0' + product.latest_price[0] : product.initial_price
+
+    return (
+        <div className={styles.card}>
+            <div className={imageClassName}>
+                <div className={styles['card-nft__creator']}>
+                    <span>created by</span>
+                    <br />
+                    {product.created_by.display_name}
+                </div>
+                <div className={styles['card-nft__name']}>{product.name}</div>
+            </div>
+            <div className={styles['card-selling']}>
+                <div>
+                    available
+                    <br />
+                    <span>
+                        {product.quantity_available} of {product.quantity}
+                    </span>
+                </div>
+                <div>
+                    price
+                    <br />
+                    <span>{price} ETH</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export const ProductCard = React.memo(ProductCardComponent)
