17d7e9be94880d15a8b67e4839d7d7f8e15ef521
Moloko20 <alexvfrontend@gmail.com>
2022-04-05 00:09:28 +0300

    perf(app): add strict mode

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 
 import { Catalog } from 'Components/Catalog'
@@ -10,4 +10,8 @@ function App() {
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>,
+)
