46a5a6fc0656729bb95efc87ed3dae76065721ef
Moloko20 <alexvfrontend@gmail.com>
2022-04-05 13:20:36 +0300

    build(webpack): fix public path

--- webpack.config.js
+++ webpack.config.js
@@ -52,7 +52,7 @@ module.exports = () => {
         },
         output: {
             path: path.resolve(__dirname, './dist'),
-            publicPath: '/',
+            publicPath: '/dist',
         },
         performance: {
             hints: false,
