3105d4c1d62ad083ed4984b380a152584fea2192
Moloko20 <alexvfrontend@gmail.com>
2022-04-05 12:06:11 +0300

    feat(catalog): add checkbox

--- src/Components/Catalog/index.tsx
+++ src/Components/Catalog/index.tsx
@@ -1,18 +1,36 @@
 import React from 'react'
 
 import { ProductList } from 'Components/ProductList'
+import { Checkbox } from 'Components/UI/Checkbox'
 
 import styles from './index.module.scss'
 
-export function Catalog() {
+function CatalogComponent() {
+    const [isChecked, isCheckedSet] = React.useState(false)
+
+    const checkboxHandler = React.useCallback(() => {
+        isCheckedSet(prevState => !prevState)
+    }, [])
+
     return (
         <section className={styles.catalog}>
-            <div className={styles['catalog_head']}>
-                <h1>Explore</h1>
-                <p>Buy and sell digital fashion NFT art</p>
+            <div className={styles['catalog-head']}>
+                <div>
+                    <h1>Explore</h1>
+                    <p>Buy and sell digital fashion NFT art</p>
+                </div>
+                <div className={styles['catalog-head_filters']}>
+                    <h2>Filters</h2>
+                    <div className={styles['catalog-head_filterwrap']}>
+                        <p>Available</p>
+                        <Checkbox checkboxHandler={checkboxHandler} />
+                    </div>
+                </div>
             </div>
 
-            <ProductList />
+            <ProductList aviable={isChecked} />
         </section>
     )
 }
+
+export const Catalog = React.memo(CatalogComponent)
